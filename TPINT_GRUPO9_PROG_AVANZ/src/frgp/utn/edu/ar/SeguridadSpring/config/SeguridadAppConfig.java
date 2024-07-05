d0da43d46b2515947982a159de4ff32c2aae3572
kylaren <86126109+kylaren@users.noreply.github.com>
2024-07-05 19:38:12 -0300

    Spring Security Config

---
+++ TPINT_GRUPO9_PROG_AVANZ/src/frgp/utn/edu/ar/SeguridadSpring/config/SeguridadAppConfig.java
@@ -0,0 +1,38 @@
+package frgp.utn.edu.ar.SeguridadSpring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SeguridadAppConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	
+		UserBuilder usuarios=User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+		.withUser(usuarios.username("admin").password("admin").roles("administador"))
+		.withUser(usuarios.username("usuario").password("123").roles("usuario"));
+	
+		
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin()
+		.loginPage("/formularioLogin")
+		.loginProcessingUrl("/autenticacionUsuario")
+		.permitAll();
+	}
+
+	
+	
+}
---
+++ TPINT_GRUPO9_PROG_AVANZ/src/frgp/utn/edu/ar/SeguridadSpring/config/SeguridadAppInicializador.java
@@ -0,0 +1,8 @@
+package frgp.utn.edu.ar.SeguridadSpring.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SeguridadAppInicializador extends AbstractSecurityWebApplicationInitializer {
+
+	
+}
